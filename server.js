416eaac7afb64b9ed4de8651c812d6da7b2da022
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-31 12:49:12 -0700

    add server.js, express,mysql2 packages

--- server.js
+++ server.js
@@ -1,1 +1,32 @@
+// Import and require express, mysql2
+const express = require("express");
+const mysql = require("mysql2");
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // MySQL username,
+    user: "root",
+    // MySQL password
+    password: "password",
+    database: "movie_db",
+  },
+  console.log(`Connected to the movie_db database.`)
+);
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
