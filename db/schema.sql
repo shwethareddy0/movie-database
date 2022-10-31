9b486c286b5cc31b618e19cb0e326e24ba5cf79f
Swetha Reddy <shwetha.reddy0@gmail.com>
2022-10-31 12:40:05 -0700

    add inital files and database

---
+++ db/schema.sql
@@ -0,0 +1,17 @@
+DROP DATABASE IF EXISTS movie_db;
+CREATE DATABASE movie_db;
+
+USE movie_db;
+
+CREATE TABLE movies(
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  movie_name VARCHAR(100)
+);
+
+CREATE TABLE reviews(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    movie_id INT,
+    FOREIGN KEY (movie_id)
+    REFERENCES movies(id),
+    review TEXT
+);
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+INSERT INTO movies(movie_name)
+VALUES  ("FROZEN"),
+        ("TROLLS"),
+        ("CARS"),
+        ("Avengers"),
+        ("Avatar");
+
+INSERT INTO reviews(movie_id, review)
+VALUES  (1, "It was good"),
+        (2, "Excellent movies with good songs"),
+        (3, "Great movie with a good message"),
+        (4, "Amazing movie"),
+        (5, "Epic movie");
+        
