f45b43a12b2b8129cb919706f3ff5ae38887a745
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-10-31 16:54:54 -0400

    2nd Get route works now

--- server.js
+++ server.js
@@ -23,25 +23,27 @@ const db = mysql.createConnection(
 );
 
 // Takes in a movie title, and inserts it into the movies table
-app.post('/api/add-movie', (req, res) => {
-  const {title} = req.body;
-  db.query(`INSERT INTO movies(movie_name) VALUES (${title})`, function (err, results) {
-    console.log(results);
-    res.send(results);
-  })
+app.post("/api/add-movie", (req, res) => {
+  const { title } = req.body;
+  db.query(
+    `INSERT INTO movies(movie_name) VALUES (${title})`,
+    function (err, results) {
+      console.log(results);
+      res.send(results);
+    }
+  );
 });
 
 // Selects movies table and console logs movies
-app.get('/api/movies', (req, res) => {
-  db.query('SELECT * FROM movies', function (err, results) {
+app.get("/api/movies", (req, res) => {
+  db.query("SELECT * FROM movies", function (err, results) {
     console.log(results);
     res.send(results);
   });
-
 });
 
 // Deletes a row based on a specified id
-app.delete('/api/movie/:id', (req, res) => {
+app.delete("/api/movie/:id", (req, res) => {
   let deletedRow = id;
   db.query(`DELETE FROM movies WHERE id = ?`, deletedRow, (err, results) => {
     if (err) {
@@ -52,49 +54,49 @@ app.delete('/api/movie/:id', (req, res) => {
   });
 });
 
-app.get('/api/movies', (req, res) => {
+app.get("/api/movies", (req, res) => {
   db.query(`SELECT * FROM movies`, function (err, results) {
     console.log(results);
     res.send(results);
   });
 });
 
-app.get('api/movie-reviews', (req, res) => {
-  db.query(`SELECT movies.movie_name AS Name, reviews.review
-  FROM movies INNER JOIN reviews
-  ON reviews.movie_id = movies.id`), function (err, results) {
-    if (err) {
-      console.log(err);
-    }
-    console.log(results);
-    res.send(results);
-  };
+app.get("/api/movie-reviews", (req, res) => {
+  db.query(
+    "SELECT movies.movie_name AS Name, reviews.review FROM movies INNER JOIN reviews ON reviews.movie_id = movies.id",
+    function (err, results) {
+      console.log(results);
+      res.json(results);
+    });
 });
 
-app.put('api/review/:id', (req , res) => {
+app.put("/api/review/:id", (req, res) => {
   let selectedMovie = id;
-  const {updatedReview} = req.body;
-  db.query(`UPDATE reviews SET review = ${updatedReview} WHERE movie_id = ${selectedMovie}`, function (err, results) {
-    if (err) {
-      console.log(err)
+  const { updatedReview } = req.body;
+  db.query(
+    `UPDATE reviews SET review = ${updatedReview} WHERE movie_id = ${selectedMovie}`,
+    function (err, results) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(results);
+        res.send(results);
+      }
     }
-    else {
-      console.log(results)
-      res.send(results);
-    }
-  })
-})
+  );
+});
 
-app.get('api/movie-reviews', (req, res) => {
+app.get("/api/movie-reviews", (req, res) => {
   db.query(`SELECT movies.id AS ID, movies.movie_name AS Name, reviews.review
   FROM movies INNER JOIN reviews
-  ON reviews.movie_id = movies.id`), function (err, results) {
-    if (err) {
-      console.log(err);
-    }
-    console.log(results);
-    res.send(results);
-  };
+  ON reviews.movie_id = movies.id`),
+    function (err, results) {
+      if (err) {
+        console.log(err);
+      }
+      console.log(results);
+      res.send(results);
+    };
 });
 
 // Default response for any other request (Not Found)
