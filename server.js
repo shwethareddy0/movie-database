785768102621d071208c738366d9a07310344fbc
PeterKim89 <112027726+PeterKim89@users.noreply.github.com>
2022-10-31 16:39:20 -0400

    server.js express functions

--- server.js
+++ server.js
@@ -22,6 +22,73 @@ const db = mysql.createConnection(
   console.log(`Connected to the movie_db database.`)
 );
 
+// Takes in a movie title, and inserts it into the movies table
+app.post('/api/add-movie', (req, res) => {
+  const {title} = req.body;
+  db.query(`INSERT INTO movies(movie_name) VALUES (${title})`, function (err, results) {
+    console.log(results);
+  })
+});
+
+// Selects movies table and console logs movies
+app.get('/api/movies', (req, res) => {
+  db.query('SELECT * FROM movies', function (err, results) {
+    console.log(results);
+  });
+});
+
+// Deletes a row based on a specified id
+app.delete('/api/movie/:id', (req, res) => {
+  let deletedRow = id;
+  db.query(`DELETE FROM movies WHERE id = ?`, deletedRow, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(result);
+  });
+});
+
+app.get('/api/movies', (req, res) => {
+  db.query(`SELECT * FROM movies`, function (err, results) {
+    console.log(results);
+  });
+});
+
+app.get('api/movie-reviews', (req, res) => {
+  db.query(`SELECT movies.movie_name AS Name, reviews.review
+  FROM movies INNER JOIN reviews
+  ON reviews.movie_id = movies.id`), function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(results);
+  };
+});
+
+app.put('api/review/:id', (req , res) => {
+  let selectedMovie = id;
+  const {updatedReview} = req.body;
+  db.query(`UPDATE reviews SET review = ${updatedReview} WHERE movie_id = ${selectedMovie}`, function (err, results) {
+    if (err) {
+      console.log(err)
+    }
+    else {
+      console.log(results)
+    }
+  })
+})
+
+app.get('api/movie-reviews', (req, res) => {
+  db.query(`SELECT movies.id AS ID, movies.movie_name AS Name, reviews.review
+  FROM movies INNER JOIN reviews
+  ON reviews.movie_id = movies.id`), function (err, results) {
+    if (err) {
+      console.log(err);
+    }
+    console.log(results);
+  };
+});
+
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
